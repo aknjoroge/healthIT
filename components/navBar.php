2aa0739edba22e76309b99f3a5ac4b54908182a1
Alexander karanja <alexkaranja629@gmail.com>
2022-02-05 22:03:53 +0300

    intalled plugin

---
+++ components/navBar.php
@@ -0,0 +1,13 @@
+<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+            <h2>Rapid Diagnostic Tests Stats</h2>
+        </li>
+    </ul>
+
+
+</nav>
---
+++ components/scripts.php
@@ -0,0 +1,25 @@
+ <!-- REQUIRED SCRIPTS -->
+
+ <!-- jQuery -->
+ <script src="plugins/jquery/jquery.min.js"></script>
+ <!-- Bootstrap -->
+ <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+ <!-- AdminLTE -->
+ <script src="dist/js/adminlte.js"></script>
+
+ <!-- OPTIONAL SCRIPTS -->
+ <script src="plugins/chart.js/Chart.min.js"></script>
+ <!-- AdminLTE for demo purposes -->
+ <script src="dist/js/demo.js"></script>
+ <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+ <script src="dist/js/pages/dashboard3.js"></script>
+
+ <!-- FLOT CHARTS -->
+ <script src="plugins/flot/jquery.flot.js"></script>
+ <!-- FLOT RESIZE PLUGIN - allows the chart to redraw when the window is resized -->
+ <script src="plugins/flot/plugins/jquery.flot.resize.js"></script>
+ <!-- FLOT PIE PLUGIN - also used to draw donut charts -->
+ <script src="plugins/flot/plugins/jquery.flot.pie.js"></script>
+ <script src="plugins/uplot/uPlot.iife.min.js"></script>
+
+ <script src="plugins/bs-custom-file-input/bs-custom-file-input.min.js"></script>
